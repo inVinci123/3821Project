024f653dd973ceef6c2ed534335dfb6120039de9
Anonymous_UserII <79725451+AnonymousUserII@users.noreply.github.com>
2025-11-18 19:44:30 +1100

    EDITED: Script to fetch metrics

--- trading_algorithms/short.sh
+++ trading_algorithms/short.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cat bullish5y | sed -n "/# $1/,/^\$/p" | grep TWorth | awk '{print $5 / $2}' | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
-cat bullish5y | sed -n "/# $1/,/^\$/p" | grep Sharpe | cut -d' ' -f4 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
-cat bullish5y | sed -n "/# $1/,/^\$/p" | grep Calmar | cut -d' ' -f3 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
-cat bullish5y | sed -n "/# $1/,/^\$/p" | grep Trade | cut -d' ' -f3 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
+def=$2
+file="${def:-bullish5y}"
+cat $file | sed -n "/# $1/,/^\$/p" | grep TWorth | awk '{print $5 / $2}' | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
+cat $file | sed -n "/# $1/,/^\$/p" | grep Sharpe | cut -d' ' -f4 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
+cat $file | sed -n "/# $1/,/^\$/p" | grep Calmar | cut -d' ' -f3 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
+cat $file | sed -n "/# $1/,/^\$/p" | grep Trade | cut -d' ' -f3 | awk 'BEGIN {a=0} {a += $1} END {print a / NR}'
